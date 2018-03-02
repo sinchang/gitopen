1697e4e5684aab1fb15a880d328afc521e10793f
sinchang <sinchangwen@gmail.com>
2018-03-02 12:30:40 +0800

    Init project

---
+++ README.md
@@ -0,0 +1,30 @@
+[![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)](https://raw.githubusercontent.com/sinchang/gitopen/master/LICENSE)
+[![Build Status](https://img.shields.io/travis/sinchang/gitopen.svg)](https://travis-ci.org/sinchang/gitopen)
+[![npm](https://img.shields.io/npm/v/gitopen.svg)](https://www.npmjs.com/package/gitopen)
+[![prettier](https://img.shields.io/badge/style-prettier-ff69b4.svg)](https://github.com/prettier/prettier)
+
+# gitopen
+
+> Open git remote url from web browser
+
+## Install
+
+```
+yarn global add @sinchang/gitopen
+```
+
+Or
+
+```
+npm install --global @sinchang/gitopen
+```
+
+## Usage
+
+```
+gitopen
+```
+
+#### License
+
+MIT © [sinchang](https://github.com/sinchang)
---
+++ index.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+
+'use strict'
+
+const execSync = require('child_process').execSync
+const cac = require('cac')
+const updateNotifier = require('update-notifier')
+const opn = require('opn')
+const pkg = require('./package.json')
+
+updateNotifier({pkg}).notify()
+
+const cli = cac()
+
+// Add a default command
+const defaultCommand = cli.command('*', {
+  desc: 'The default command'
+}, (input, flags) => {
+  try {
+    const cmd = execSync(`git remote get-url ${flags.name || 'origin'}`)
+    opn(cmd.toString().trim())
+    process.exit()
+  } catch(err) {
+    console.error(err)
+    process.exit(1)
+  }
+})
+
+defaultCommand.option('name', {
+  desc: 'tell me the remote name'
+})
+
+// Bootstrap the CLI app
+cli.parse()
