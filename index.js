7edbcbbfab9c06d53c8cb23fbaca324dde1fa2b4
sinchang <sinchangwen@gmail.com>
2018-03-02 12:43:51 +0800

    Fix lint

--- index.js
+++ index.js
@@ -1,33 +1,37 @@
 #!/usr/bin/env node
-
 'use strict'
 
-const execSync = require('child_process').execSync
-const cac = require('cac')
-const updateNotifier = require('update-notifier')
-const opn = require('opn')
-const pkg = require('./package.json')
+const execSync = require(`child_process`).execSync
+const cac = require(`cac`)
+const updateNotifier = require(`update-notifier`)
+const opn = require(`opn`)
+const pkg = require(`./package.json`)
 
-updateNotifier({pkg}).notify()
+updateNotifier({ pkg }).notify()
 
 const cli = cac()
 
 // Add a default command
-const defaultCommand = cli.command('*', {
-  desc: 'The default command'
-}, (input, flags) => {
-  try {
-    const cmd = execSync(`git remote get-url ${flags.name || 'origin'}`)
-    opn(cmd.toString().trim())
-    process.exit()
-  } catch(err) {
-    console.error(err)
-    process.exit(1)
-  }
-})
+const defaultCommand = cli.command(
+  `*`,
+  {
+    desc: `The default command`
+  },
+  (input, flags) => {
+    try {
+      const cmd = execSync(`git remote get-url ${flags.name || `origin`}`)
 
-defaultCommand.option('name', {
-  desc: 'tell me the remote name'
+      opn(cmd.toString().trim())
+      process.exit()
+    } catch (err) {
+      console.error(err.message)
+      process.exit(1)
+    }
+  }
+)
+
+defaultCommand.option(`name`, {
+  desc: `Tell me the remote name`
 })
 
 // Bootstrap the CLI app
